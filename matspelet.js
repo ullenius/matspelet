5289ecef3cb08baa349858772bd20e01eb4047b3
Anosh D. Ullenius <anosh@anosh.se>
2020-06-17 20:07:16 +0200

    Moved data to separate file (export/import

--- matspelet.js
+++ matspelet.js
@@ -6,7 +6,7 @@ I "Matspelet" får användaren svara på fem frågor om mat.
 Användaren får 10 sekunder per fråga att svara.
 */
 "use strict";
-
+import questions from "./questions.js";
 let randomQuestions;
 let currentQuestion;
 let setIntervalID;
@@ -21,135 +21,6 @@ function init() {
 }
 
 function getRandomQuestions() {
-	
-	const questions = [
-		{
-			"level" : 1,
-			"question" : "Från vilket land kommer såsen Béarnaise?",
-			"answer" : "Frankrike"
-		},
-		{
-			"level" : 1,
-			"question" : "James Bond dricker ofta en drink gjord på gin och vermouth. Vad heter drinken?",
-			"answer" : "Dry Martini"
-		},
-		{
-			"level" : 1,
-			"question" : "Vad är huvudingrediensen i en omelett?",
-			"answer" : "Ägg"
-		},
-		{
-			"level" : 1,
-			"question" : "Vilken är världens dyraste krydda?",
-			"answer" : "Saffran"
-		},
-		{
-			"level" : 1,
-			"question" : "Vad kallas den tomatbaserade grönsakssåsen som ofta äts med tacos och nachos?",
-			"answer" : "Salsa"
-		},
-		{
-			"level" : 2,
-			"question" : "Vad heter en känd italiensk sås som innehåller basilika, vitlök, olivolja, parmesan och pinjenötter?",
-			"answer" : "Pesto"
-		},
-		{
-			"level" : 2,
-			"question" : "Vad heter pizzan som bara innehåller tomatsås och ost (och ibland även basilika)?",
-			"answer" : "Margherita"
-		},
-		{
-			"level" : 2,
-			"question" : "Vilken typ av mat är en salsiccia?",
-			"answer" : "Korv"
-		},
-		{
-			"level" : 2,
-			"question" : "Vad kallas den asiatiska sojabönsmassan som är proteinrik och nästan smaklös?",
-			"answer" : "Tofu"
-		},
-		{
-			"level" : 2,
-			"question" : "Från vilken del av Sverige härstammar saffranspannkaka?",
-			"answer" : "Gotland"
-		},
-		
-		{
-			"level" : 3,
-			"question" : "Vilken grönsak är viktig i en moussaka?",
-			"answer" : "Aubergine"
-		},
-		{
-			"level" : 3,
-			"question" : "Vad heter rötten från Skottland som görs med lever, inälvor, lök och havregryn?",
-			"answer" : "Haggis"
-		},
-		{
-			"level" : 3,
-			"question" : "Vilken är huvudingrediensen i kroppkakor?",
-			"answer" : "Potatis"
-		},
-		{
-			"level" : 3,
-			"question" : "Från vilket djurs mjölk gör man chévreost?",
-			"answer" : "Get"
-		},
-		{
-			"level" : 3,
-			"question" : "Vilken krydda ger currypulver dess gula färg?",
-			"answer" : "Gurkmeja"
-		},
-		{
-			"level" : 4,
-			"question" : "Matkedjan KFC är specialiserade på snabbmat av en viss typ av djur. Vilket?",
-			"answer" : "Kyckling"
-		},
-		{
-			"level" : 4,
-			"question" : "Paul Giamattis karaktär i filmen Sideways är förtjust i en viss typ av vindruva. Vilken?",
-			"answer" : "Pinot Noir"
-		},
-		{
-			"level" : 4,
-			"question" : "Vilken typ av öl görs det mest av i England?",
-			"answer" : "Ale"
-		},
-		{
-			"level" : 4,
-			"question" : "Vilken ingrediens, förutom skinka, har huvudrollen i en Quiche Lorraine?",
-			"answer" : "Ost"
-		},
-		{
-			"level" : 4,
-			"question" : "Vad kallas den inlagda ingefäran som brukar ätas i samband med sushi?",
-			"answer" : "Gari"
-		},
-		{
-			"level" : 5,
-			"question" : "Vad betyder det hindiska ordet masala?",
-			"answer" : "Kryddblandning"
-		},
-		{
-			"level" : 5,
-			"question" : "Vilken typ av dryck är geuze?",
-			"answer" : "Öl"
-		},
-		{
-			"level" : 5,
-			"question" : "Vad är det riktiga namnet för söt matlagningssake?",
-			"answer" : "Mirin"
-		},
-		{
-			"level" : 5,
-			"question" : "Vilken grön druva ger viner som passar till het mat med asiatiska inslag?",
-			"answer" : "Riesling"
-		},
-		{
-			"level" : 5,
-			"question" : "Vad kallas jalapñeo som har blivit rökt och torkad?",
-			"answer" : "Chipotle"
-		}
-	];
 	
 	const mySet = getLevels(questions);
 	const sortedLevels = sortLevels(mySet);
@@ -219,18 +90,15 @@ function visaFraga()
 {
 	let counter = 10;
 	
-	const question = nextQuestion();
-	console.log(question);
-	displayQuestion("Fråga " + question.level + ": " + question.question);
-	setIntervalID = setInterval(countdown, 1000); // hoisting... again!
+	nextQuestion();
+	console.log(currentQuestion);
+	displayQuestion("Fråga " + currentQuestion.level + ": " + currentQuestion.question);
+	setIntervalID = setInterval(countdown, 1000); //global variable
 	
 	function nextQuestion() {
-		const nextQuestion = randomQuestions.shift();
-		Object.freeze(nextQuestion);
-		currentQuestion = nextQuestion;
-		return nextQuestion;
+		currentQuestion = randomQuestions.shift();
+		return currentQuestion;
 	}
-	
 	function countdown()
 	{
 		displayTime("Tid kvar: " + counter--);
@@ -260,15 +128,13 @@ function displayTime(time)
  */
 function svara() // FIXME refactor this function
 {
-		const svar = document.getElementById("svar");
-		const question = currentQuestion;
+		const answer = getInput();
+		const correctAnswer = checkAnswer(answer, currentQuestion);
 		console.log("current question");
-		console.log(question.answer);
+		console.log(currentQuestion);
 		
-		const rattSvar = equalsIgnoreCase(svar.value, question.answer);
-		
-		if (rattSvar === true) {
-			svar.value = "";
+		if (correctAnswer === true) {
+			answer = "";
 			clearInterval(setIntervalID);
 
 			if (lastQuestion() === true) {
@@ -278,14 +144,20 @@ function svara() // FIXME refactor this function
 				visaFraga();
 			}
 		}
-		else if (rattSvar === false) {
+		else if (correctAnswer === false) {
 			gameOver("Fel svar! Försök igen...");
 		}
-		
-	function equalsIgnoreCase(first, second) {
-		return (first.toLowerCase() === second.toLowerCase()) ? true : false;
-	}
 	
+	function getInput() {
+		return document.getElementById("svar").value;
+	}
+	function checkAnswer(answer, current) {
+		return equalsIgnoreCase(answer, current.answer);
+		
+		function equalsIgnoreCase(first, second) {
+			return (first.toLowerCase() === second.toLowerCase()) ? true : false;
+		}
+	}
 	function lastQuestion() {
 		return (randomQuestions.length === 0) ? true : false;
 	}
@@ -293,7 +165,6 @@ function svara() // FIXME refactor this function
 
 function gameOver(message = "") {
 	init();
-	
 	clearInterval(setIntervalID);
 	const textNodeResultat = document.createTextNode(message);
 	document.getElementById("resultat").appendChild(textNodeResultat);
